88eccaafa7c222fcaff288324d8dbf1e3bf85f18
Radprzem <114361982+Radprzem@users.noreply.github.com>
2023-08-22 22:10:26 +0200

    Add files via upload

---
+++ huh_fish.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+	    cout << "              huh" << endl;
+	    cout << "|\\    \\ \\ \\ \\ \\ \\ \\      __   " << endl;
+	    cout << "|  \\    \\ \\ \\ \\ \\ \\ \\   | O~-_" << endl;
+	    cout << "|   >----|-|-|-|-|-|-|--|  __/" << endl;
+	    cout << "|  /    / / / / / / /   |__\\  " << endl;
+	    cout << "|/     / / / / / / /" << endl;
+	    usleep(1000000);
+	    system("clear");
+	    cout << "              huh" << endl;
+	    cout << "   __      / / / / / / / /    /|" << endl;
+	    cout << "_-~0 |   / / / / / / / /    /  |" << endl;
+	    cout << "\\__  |--|-|-|-|-|-|-|-|----<   |" << endl;
+	    cout << "  /__|   \\ \\ \\ \\ \\ \\ \\ \\    \\  |" << endl;
+	    cout << "          \\ \\ \\ \\ \\ \\ \\ \\     \\|" << endl;
+	    usleep(1000000);
+	    system("clear");
+	}
+	return 0;
+}
